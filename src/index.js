691baa752a0885d1fae149f8fbedd162185aa658
Charbel Hbayter <charbel.hbayter@lau.edu>
2021-04-27 15:44:40 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 let addToy = false;
+let mDiv;
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -12,4 +13,34 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  
+ 
+  ft();
+  
 });
+function ft(){
+    fetch("http://localhost:3000/toys")
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (json) {
+      console.log(json);
+      //displayDiv(json);
+    });
+}
+function displayDiv(json) {
+  mDiv=document.getElementById("toy-collection");
+  console.log(json.data.length);
+  for (let i = 0; i < json.data.length; i++) {
+    imagesDiv.innerHTML += `
+    <img src=${json.data[i].images.original.url}/>
+    `;
+  }
+}
+  
+
+
+
+
+
+
